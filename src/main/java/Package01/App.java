fe63be7e1891aaf374673a96c2829f735558eb2d
Vansh Rajput <89916310+vanshrajput01@users.noreply.github.com>
2024-10-16 17:02:07 +0530

    Main class code add

---
+++ src/main/java/Package01/App.java
@@ -0,0 +1,25 @@
+package com.temp.Hibernate_Project;
+
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+import org.hibernate.cfg.Configuration;
+
+public class App 
+{
+    public static void main( String[] args )
+    {
+        SessionFactory factory = new Configuration().configure("hibernate.cfg.xml").buildSessionFactory();
+        Session session = factory.openSession();
+        Student st = new Student();
+        st.setStudName("student03");
+        st.setStudFee(15000);
+        Transaction trx = session.beginTransaction();
+        session.save(st);
+        trx.commit();
+        System.out.println("data add successFully!!");
+        session.close();
+        
+    }
+}
