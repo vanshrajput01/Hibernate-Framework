18c7571f0e00276f744cbbf504206241311b5cab
Vansh Rajput <89916310+vanshrajput01@users.noreply.github.com>
2024-10-16 17:46:55 +0530

    get data from table by id

--- src/main/java/Package01/App.java
+++ src/main/java/Package01/App.java
@@ -10,9 +10,9 @@ public class App
 {
     public static void main( String[] args )
     {
+         // Save student object (means add data in student table).
         SessionFactory factory = new Configuration().configure("pass here url of hibernate configuration file.").buildSessionFactory();
         Session session = factory.openSession();
-        // Save student object (means add data in student table).
         Student st = new Student();
         st.setStudName("student03");
         st.setStudFee(15000);
@@ -21,6 +21,17 @@ public class App
         trx.commit();
         System.out.println("data add successFully!!");
         session.close();
+        
+        // if you want to get data from  Table by id..
+
+        SessionFactory factory = new Configuration().configure("hibernate.cfg.xml").buildSessionFactory();
+    	Session session = factory.openSession();
+    	// if you can get data by id..
+    	Student st = session.get(Student.class, 1);
+    	System.out.println("Stuednt deatils is :-" + st.getStudName() + " " + st.getStudFee());
+    	session.close();
+
+        // if you get all data from table...
         
     }
 }
