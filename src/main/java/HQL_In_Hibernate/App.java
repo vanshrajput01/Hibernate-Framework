28476cec7504b92c60bb4e078f67d7eedb55005b
Vansh Rajput <89916310+vanshrajput01@users.noreply.github.com>
2024-10-18 10:35:12 +0530

    get all data in database using HQL language.

--- src/main/java/HQL_In_Hibernate/App.java
+++ src/main/java/HQL_In_Hibernate/App.java
@@ -10,14 +10,28 @@ public class App
 {
     public static void main( String[] args )
     {
+        //  How to get single row in database using HQL language. 
     	SessionFactory factory = new Configuration().configure("hibernate.cfg.xml").buildSessionFactory();
     	Session session = factory.openSession();
-//    	How to get single row in database using HQL language. 
     	String q = "from EmployeeEntity where empId = :id";
     	Query query = session.createQuery(q);
     	query.setParameter("id", 2);
     	EmployeeEntity emp = (EmployeeEntity)query.uniqueResult();
     	System.out.println("Employee details :- "+ emp.getEmpId() + " " + emp.getEmpName() + " " + emp.getEmpSalary());
+    	session.close();
+
+        //  How to get all data in database using HQL language. 
+        SessionFactory factory = new Configuration().configure("hibernate.cfg.xml").buildSessionFactory();
+    	Session session = factory.openSession();
+    	String q = "from EmployeeEntity";
+    	Query query = session.createQuery(q);
+    	List<EmployeeEntity> empList = query.list();
+    	for (EmployeeEntity emp : empList) {
+    		System.out.println(" **** employee details *****");
+    		System.out.println("emp id" + emp.getEmpId() + "data is :- " + emp.getEmpSalary() + " " +  emp.getEmpId());
+    	}
+    	
+
     	session.close();
 
       
