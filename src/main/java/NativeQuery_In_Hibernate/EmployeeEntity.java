f9dcb512ad827d8ec5e462be5cc29f1095b6965c
Vansh Rajput <89916310+vanshrajput01@users.noreply.github.com>
2024-10-18 15:26:25 +0530

    Create Employee Entity class

---
+++ src/main/java/NativeQuery_In_Hibernate/EmployeeEntity.java
@@ -0,0 +1,43 @@
+
+package com.hibernateFramework.Entity;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "employee_details")
+public class EmployeeEntity {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "emp_id")
+	private Integer empId;
+	@Column(name = "emp_name")
+	private String empName;
+	@Column(name = "emp_salary")
+	private Integer empSalary;
+	public Integer getEmpId() {
+		return empId;
+	}
+	public void setEmpId(Integer empId) {
+		this.empId = empId;
+	}
+	public String getEmpName() {
+		return empName;
+	}
+	public void setEmpName(String empName) {
+		this.empName = empName;
+	}
+	public Integer getEmpSalary() {
+		return empSalary;
+	}
+	public void setEmpSalary(Integer empSalary) {
+		this.empSalary = empSalary;
+	}
+	
+	
+
+}
